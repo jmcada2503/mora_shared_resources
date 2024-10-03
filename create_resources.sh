3358656172abd639a929105e1f41c8c101cd0dcb
jcastaneda30 <jaidercasta30@gmail.com>
2024-10-03 16:29:14 -0500

    add localstak@

---
+++ create_resources.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Configuración de los parámetros para S3 y SQS
+BUCKET_NAME="undi-documents"
+LOCALSTACK_URL="http://localhost:4566"
+
+# Función para crear el bucket en S3
+create_s3_bucket() {
+  echo "Creando el bucket de S3: $BUCKET_NAME"
+  awslocal --endpoint-url=$LOCALSTACK_URL s3api create-bucket --bucket $BUCKET_NAME
+  if [ $? -eq 0 ]; then
+    echo "Bucket $BUCKET_NAME creado exitosamente."
+  else
+    echo "Error al crear el bucket $BUCKET_NAME."
+  fi
+}
+
+
+# Ejecutar las funciones
+create_s3_bucket
+#create_sqs_queue
+
+echo "Todos los recursos han sido creados."
