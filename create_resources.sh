1047e73bd4e17174e2691c368f94fb2d3e443ab1
jcastaneda30 <jaidercasta30@gmail.com>
2024-10-13 07:39:13 -0500

    automatic create queues and bucket

--- create_resources.sh
+++ create_resources.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
-# Configuración de los parámetros para S3 y SQS
-BUCKET_NAME="undi-documents"
+# Espera hasta que el servicio de SQS esté disponible
 LOCALSTACK_URL="http://localhost:4566"
 
-# Función para crear el bucket en S3
+# Función para crear un bucket en S3
 create_s3_bucket() {
+  BUCKET_NAME=$1
+  if [ -z "$BUCKET_NAME" ]; then
+    echo "Debe proporcionar un nombre de bucket."
+    exit 1
+  fi
   echo "Creando el bucket de S3: $BUCKET_NAME"
   awslocal --endpoint-url=$LOCALSTACK_URL s3api create-bucket --bucket $BUCKET_NAME
   if [ $? -eq 0 ]; then
@@ -15,11 +18,26 @@ create_s3_bucket() {
   fi
 }
 
+# Función para crear una cola en SQS
+create_sqs_queue() {
+  QUEUE_NAME=$1
+  if [ -z "$QUEUE_NAME" ]; then
+    echo "Debe proporcionar un nombre de cola."
+    exit 1
+  fi
+  echo "Creando la cola de SQS: $QUEUE_NAME"
+  awslocal --endpoint-url=$LOCALSTACK_URL sqs create-queue --queue-name $QUEUE_NAME
+  if [ $? -eq 0 ]; then
+    echo "Cola $QUEUE_NAME creada exitosamente."
+  else
+    echo "Error al crear la cola $QUEUE_NAME."
+  fi
+}
 
-# Ejecutar las funciones
-create_s3_bucket
-#create_sqs_queue
-awslocal sqs create-queue --queue-name coreapi-queue
-awslocal sqs create-queue --queue-name coreapi-load-queue
+# Ejecutar las funciones pasándole los parámetros como argumentos
+create_s3_bucket "undi-documents"
+create_sqs_queue "calculations-queue"
+create_sqs_queue "coreapi-queue"
+create_sqs_queue "coreapi-load-queue"
 
 echo "Todos los recursos han sido creados."
